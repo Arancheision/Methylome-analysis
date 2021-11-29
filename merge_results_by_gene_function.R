6d840a686fa044ed36b18ae90d669cd05209aa78
Arancheision <arancheision@gmail.com>
2021-11-29 12:14:38 +0100

    Add files via upload

--- merge_results_by_gene_function.R
+++ merge_results_by_gene_function.R
@@ -12,25 +12,25 @@ merge.by.gene<- function ( MEDIPS.object ){
   
   #Merge by gene and take the mean of the value
   result.merge <- aggregate.data.frame( MEDIPS.object[,4:14], 
-                                        by= list(MEDIPS.object$gene), 
+                                        by= list(MEDIPS.object$ROI), 
                                         FUN= mean)
   
   #Map those regions to their chromosome
   merge.chr <- aggregate.data.frame( MEDIPS.object$chr, 
-                                     by= list(MEDIPS.object$gene), 
+                                     by= list(MEDIPS.object$ROI), 
                                      FUN= unique)
   
   #Take the start of the first window and the stop of the last window merged
   merge.start <- aggregate.data.frame( MEDIPS.object$start, 
-                                       by= list(MEDIPS.object$gene), 
+                                       by= list(MEDIPS.object$ROI), 
                                        FUN= min)
   
   merge.stop <- aggregate.data.frame( MEDIPS.object$stop, 
-                                      by= list(MEDIPS.object$gene),
+                                      by= list(MEDIPS.object$ROI),
                                       FUN= max)
   
-  merge.windows<- aggregate.data.frame( MEDIPS.object$gene, 
-                                        by= list(MEDIPS.object$gene), 
+  merge.windows<- aggregate.data.frame( MEDIPS.object$ROI, 
+                                        by= list(MEDIPS.object$ROI), 
                                         FUN= length)
   
   #sanity check:
@@ -54,10 +54,13 @@ merge.by.gene<- function ( MEDIPS.object ){
   
   result.merge <- result.merge[order(result.merge$edgeR.logFC, 
                                      decreasing = TRUE),]
-  #names(result.merge)[1]<- "gene"
+  #Give meaningful names
+  names(result.merge)[1] <- "gene"
   
-  #return<- result.merge[, c(11:14,1:10)] #put columns in a logical order
+  #sort columns in a logical order
+  result.merge <- result.merge[,c(13:16, 1:12)]
 
-  return<- result.merge
+  return<- result.merge 
+
 }
 
